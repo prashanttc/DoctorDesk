4c9ad6968fe31249c61591791e5503192e125a3e
prashant <prashantchouhan711@gmail.com>
2025-01-16 22:10:54 +0530

    s

--- app/patients/[userId]/new-appointment/page.tsx
+++ app/patients/[userId]/new-appointment/page.tsx
@@ -1,31 +1,46 @@
 import AppointmentForm from '@/components/forms/AppointmentForm';
 import { getPatient } from '@/lib/actions/patient.actions';
-import Image from 'next/image'
+import Image from 'next/image';
 import Link from 'next/link';
-import React from 'react'
+import React from 'react';
 
 export const dynamic = 'force-dynamic';
 
+type SearchParamProps = {
+  params: {
+    userId: string;
+  };
+};
+
 const NewAppointment = async ({ params: { userId } }: SearchParamProps) => {
-  const patient = await getPatient(userId)
+  const patient = await getPatient(userId);
   return (
     <div className="flex h-screen max-h-screen remove-scrollbar">
       <section className="container remove-scrollbar">
-        <div className="sub-container max-w-[860px] flex-1 py-10 justify-between ">
-          <Link href='/'>
-            <Image src='/assets/icons/logo-full.svg' height={1000} width={1000} alt="logo" className="mb-10 h-12 w-fit" />
+        <div className="sub-container max-w-[860px] flex-1 py-10 justify-between">
+          <Link href="/">
+            <Image
+              src="/assets/icons/logo-full.svg"
+              height={1000}
+              width={1000}
+              alt="logo"
+              className="mb-10 h-12 w-fit"
+              priority
+            />
           </Link>
           <AppointmentForm userId={userId} type="create" patientId={patient.$id} />
-          <p className="copyright py-12">
-            © 2024 carePulse
-          </p>
+          <p className="copyright py-12">© 2024 carePulse</p>
         </div>
       </section>
-      <Image src='/assets/images/appointment-img.png' height={1000} width={1000} alt="appointment " className="side-img max-w-[300px] bg-bottom" />
-
+      <Image
+        src="/assets/images/appointment-img.png"
+        height={1000}
+        width={1000}
+        alt="appointment"
+        className="side-img max-w-[300px] bg-bottom"
+      />
     </div>
+  );
+};
 
-  )
-}
-
-export default NewAppointment
+export default NewAppointment;
--- types/index.d.ts
+++ types/index.d.ts
@@ -2,7 +2,7 @@
 
 declare type SearchParamProps = {
     params: { [key: string]: string };
-    searchParams: { [key: string]: string | string[] | undefined };
+    searchParams?: { [key: string]: string | string[] | undefined };
   };
   
   declare type Gender = "Male" | "Female" | "Other";
